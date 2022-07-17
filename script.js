bc6ad2b7907457e9a664d3c3a7de3d73e99e9b5f
ATEOTG <bryansantos9797@gmail.com>
2022-07-17 16:46:48 -0400

    added animation of the arrow icon

--- script.js
+++ script.js
@@ -11,22 +11,22 @@ const iconUp2 = document.querySelector(".icon-up2");
 
 // event listener
 
-features.addEventListener("mouseover", function () {
-  iconDown1.style.display = "none";
-  iconUp1.style.display = "inline-block";
-});
+// features.addEventListener("mouseover", function () {
+//   iconDown1.style.display = "none";
+//   iconUp1.style.display = "inline-block";
+// });
 
-features.addEventListener("mouseout", function () {
-  iconUp1.style.display = "none";
-  iconDown1.style.display = "inline-block";
-});
+// features.addEventListener("mouseout", function () {
+//   iconUp1.style.display = "none";
+//   iconDown1.style.display = "inline-block";
+// });
 
-company.addEventListener("mouseover", function () {
-  iconDown2.style.display = "none";
-  iconUp2.style.display = "inline-block";
-});
+// company.addEventListener("mouseover", function () {
+//   iconDown2.style.display = "none";
+//   iconUp2.style.display = "inline-block";
+// });
 
-company.addEventListener("mouseout", function () {
-  iconUp2.style.display = "none";
-  iconDown2.style.display = "inline-block";
-});
+// company.addEventListener("mouseout", function () {
+//   iconUp2.style.display = "none";
+//   iconDown2.style.display = "inline-block";
+// });
